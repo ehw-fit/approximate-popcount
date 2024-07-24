1101608be1eed63dffb2c364fca47c6565037164
Vojtech Mrazek <mrazek@fit.vutbr.cz>
2024-07-24 09:55:56 +0200

    gitignore, new data

---
+++ pcc-circuits/pcc_1_4/breastcancer_6/breastcancer_6_0002.v
@@ -0,0 +1,31 @@
+
+module cmp_pos(input [0:0] a, output [0:0] popcnt_out);
+
+
+  assign popcnt_out[0] = a;
+endmodule
+
+module cmp_neg(input [3:0] input_a, output [2:0] cgp_out);
+  wire cgp_core_006;
+  wire cgp_core_009;
+  wire cgp_core_010;
+  wire cgp_core_012;
+
+  assign cgp_core_006 = ~input_a[1];
+  assign cgp_core_009 = ~input_a[3];
+  assign cgp_core_010 = input_a[3] ^ input_a[0];
+  assign cgp_core_012 = ~input_a[2];
+
+  assign cgp_out[0] = input_a[1];
+  assign cgp_out[1] = 1'b1;
+  assign cgp_out[2] = 1'b0;
+endmodule
+module pcc(input [0:0] pos, input [3:0] neg, output outval);
+    wire [0:0] cnt_pos;
+    wire [1:0] cnt_neg;
+
+    cmp_pos ipos(pos, cnt_pos);
+    cmp_neg ineg(neg, cnt_neg);
+
+    assign outval = (cnt_pos >= cnt_neg);
+endmodule
---
+++ pcc-circuits/pcc_2_3/redwine_0/redwine_0_0005.v
@@ -0,0 +1,27 @@
+
+module cmp_pos(input [1:0] input_a, output [1:0] cgp_out);
+  wire cgp_core_004;
+  wire cgp_core_005;
+
+  assign cgp_core_004 = input_a[0] ^ input_a[1];
+  assign cgp_core_005 = input_a[0] & input_a[1];
+
+  assign cgp_out[0] = cgp_core_004;
+  assign cgp_out[1] = cgp_core_005;
+endmodule
+
+module cmp_neg(input [2:0] input_a, output [1:0] cgp_out);
+
+
+  assign cgp_out[0] = input_a[0];
+  assign cgp_out[1] = input_a[1];
+endmodule
+module pcc(input [1:0] pos, input [2:0] neg, output outval);
+    wire [0:0] cnt_pos;
+    wire [1:0] cnt_neg;
+
+    cmp_pos ipos(pos, cnt_pos);
+    cmp_neg ineg(neg, cnt_neg);
+
+    assign outval = (cnt_pos >= cnt_neg);
+endmodule
